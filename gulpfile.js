dfa8f751df1b8a61a915f2cb05ea13fb952682f6
Niels Mentink <niels@ment.ink>
2019-12-18 20:53:03 +0100

    Added better comments and changed to build folder

--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,36 @@
-const gulp = require('gulp');
-const sass = require('gulp-sass');
-const uglify = require('gulp-uglify-es').default;
-const del = require('del');
+const gulp = require("gulp");
+const sass = require("gulp-sass");
+const uglify = require("gulp-uglify-es").default;
+const del = require("del");
 
-// Separate tasks
-gulp.task('sassCompile', function() {
-  return gulp.src('./resources/sass/*.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./public/css'))
+// Compile Sass to CSS
+gulp.task("sassCompile", function() {
+  return gulp
+    .src("./resources/sass/*.scss")
+    .pipe(sass().on("error", sass.logError))
+    .pipe(gulp.dest("./public/build/css"));
 });
 
-gulp.task('jsCompress', function() {
-  return gulp.src('./resources/js/*.js')
+// Compress JS files to Build
+gulp.task("jsCompress", function() {
+  return gulp
+    .src("./resources/js/*.js")
     .pipe(uglify())
-    .pipe(gulp.dest('./public/js'))
+    .pipe(gulp.dest("./public/build/js"));
 });
 
-gulp.task('jsUncompress', function() {
-  return gulp.src('./resources/js/*.js')
-    .pipe(del('public/js', {force:true}))
-    .pipe(gulp.dest('./public/js'))
+// Uncompress JS files from Build for debugging
+gulp.task("jsUncompress", function() {
+  return gulp
+    .src("./resources/js/*.js")
+    .pipe(del("public/build/js", { force: true }))
+    .pipe(gulp.dest("./public/build/js"));
 });
 
-gulp.task('clean', function(){
-  return del('public/**', {force:true});
+// Cleans Build folder
+gulp.task("clean", function() {
+  return del("public/build/**", { force: true });
 });
 
-// Combined tasks
-gulp.task('default', gulp.series('clean','sassCompile', 'jsCompress'));
+// Default task
+gulp.task("default", gulp.series("clean", "sassCompile", "jsCompress"));
