da362891c52cf950ab7e0c11c10f5c28e681278b
Niels Mentink <vakantie@socialbrothers.nl>
2019-12-18 12:37:15 +0100

    Database connectie werktgit status

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const app = express();
 const port = 5000;
 
 const db = mysql.createConnection({
-  host: "aii.databases.avans.nl",
+  host: "databases.aii.avans.nl",
   user: "nmentink",
   password: "toor"
 });
@@ -18,3 +18,15 @@ db.connect(err => {
   console.log("Connected to database");
 });
 global.db = db;
+
+app.listen(port, () => {
+  console.log(`Server running on port: ${port}`);
+});
+
+db.query("SELECT * FROM mrchrzan_db2.product", function(err, dbres) {
+  if (err) {
+    throw err;
+  } else {
+    console.log(dbres);
+  }
+});
