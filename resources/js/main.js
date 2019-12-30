d429cadf7d8a71010cc36e28c933917bfc942dcb
Niels Mentink <niels@ment.ink>
2019-12-30 02:45:06 +0100

    Made sidebar stay open or closed based on cookie

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,7 +1,20 @@
 feather.replace();
 
+if (Cookies.getJSON("closed")) {
+  Cookies.set("closed", true);
+  $(".wrapper").addClass("closed");
+} else if (typeof Cookies.getJSON("closed") == undefined) {
+  Cookies.set("closed", false);
+} else {
+  Cookies.set("closed", false);
+  $(".wrapper").removeClass("closed");
+}
+
 $(document).ready(function() {
+  $(".wrapper").removeClass("notransition");
+
   $(".sidebartoggle").click(function() {
+    Cookies.set("closed", !Cookies.getJSON("closed"));
     $(".wrapper").toggleClass("closed");
   });
 });
